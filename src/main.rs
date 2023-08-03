c25a37a979e1131a553de9f3d579a4bf4b26a976
lukaszgorka2010 <lukaszgorka2010@gmail.com>
2023-08-03 02:58:15 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-use pallet::Pallet;
 mod pallet;
 
 fn main() {
-    let mut plt = pallet::add(12345554353453454345, 21372173, 220, "A2137".to_string());
-    println!("{:#?}", &plt);
 
-    plt.update_quantity(110);
-    println!("{:#?}", &plt);
-    
-    plt.update_slot("P2137".to_string());
-    println!("{:#?}", &plt);
 }
