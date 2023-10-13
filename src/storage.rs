22a6bf9435bfe18a58b4b3750c4dc93d2c11a2cb
lukaszgorka2010 <lukaszgorka2010@gmail.com>
2023-10-13 01:47:23 +0100

    improved storage::Storage::get_free_id

--- src/storage.rs
+++ src/storage.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-use rand::Rng;
-
 use crate::{pallet::{Pallet, self}, slots::{Slots, Slot}};
 
 const DUMMY_SLOT_ID: &str = "X9999";
@@ -13,12 +11,15 @@ pub enum StorageError {
 }
 
 #[derive(Debug, Default)]
-pub struct Storage(HashMap<u64, Pallet>);
+pub struct Storage {
+    pallets: HashMap<u64, Pallet>,
+    next_id: u64,
+}
 
 impl Storage {
     pub fn add_pallet(&mut self, sku: u32, quantity: u16, did: u32) -> u64 {
         let id = self.get_free_id(did);
-        self.0.insert(id, Pallet::create(id, sku, quantity));
+        self.pallets.insert(id, Pallet::create(id, sku, quantity));
         id
     }
 
@@ -26,24 +27,15 @@ impl Storage {
         let id = self.add_pallet(sku, quantity, did);
         let slot = slots.get_empty().unwrap_or(Slot::new(DUMMY_SLOT_ID));
         slots.assign_pallet(slot.to_owned(), id);
-        self.0.get_mut(&id).unwrap().change_to_awaiting_putaway(slot);
+        self.pallets.get_mut(&id).unwrap().change_to_awaiting_putaway(slot);
     }
     pub fn putaway(&mut self, pallet_id: u64) -> Result<(), StorageError> {
-        /*let pallet = self.0.get_mut(&pallet_id).unwrap();
-        match pallet.clone().status() {
-            pallet::Status::AwaitingPutaway(slot) => {
-                pallet.update_slot(slot);
-                pallet.change_to_available();
-                Ok(())
-            },
-            _ => Err(StorageError::Putaway),
-        }*/
-        let pallet_status = self.0.get(&pallet_id)
+        let pallet_status = self.pallets.get(&pallet_id)
                                             .map(|p| p.status().clone())
                                             .ok_or(StorageError::Putaway)?;
 
         if let pallet::Status::AwaitingPutaway(slot) = pallet_status {
-            if let Some(pallet) = self.0.get_mut(&pallet_id) {
+            if let Some(pallet) = self.pallets.get_mut(&pallet_id) {
                 pallet.update_slot(&slot);
                 pallet.change_to_available();
                 return Ok(());
@@ -52,15 +44,10 @@ impl Storage {
         Err(StorageError::Putaway)
     }
 
-    fn get_free_id(&self, did: u32) -> u64 {
-        let mut rng = rand::thread_rng();
-        loop{
-            let tail: u64 = rng.gen_range(0..=99999999999999);
-            let candidate_id: u64 = did as u64 * 10_u64.pow(15) + tail;
-            if !self.0.contains_key(&candidate_id) {
-                return candidate_id;
-            }
-        }
-        
+    fn get_free_id(&mut self, did: u32) -> u64 {
+        let candidate_id= did as u64 * 10_u64.pow(15) + self.next_id;
+        self.next_id += 1;
+        candidate_id
     }
+        
 }
